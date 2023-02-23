aa225ee392ec6fec9ebd67218f6e316070574067
neallseth <32910906+neallseth@users.noreply.github.com>
2023-02-22 19:17:51 -0500

    init

---
+++ bin/ascii.js
@@ -0,0 +1,59 @@
+export const moonPhaseArt = {
+  "Full Moon": `      _..._     
+    .:::::::.    
+   :::::::::::   Full Moon
+   ::::::::::: 
+   \`:::::::::'  
+     \`':::'' 
+  `,
+  "Waning Gibbous": `      _..._     
+    .::::. \`.    
+   :::::::.  :    Waning Gibbous
+   ::::::::  :  
+   \`::::::' .'  
+     \`'::'-' 
+  `,
+  "Last Quarter": `      _..._     
+    .::::  \`.    
+   ::::::    :    Last Quarter
+   ::::::    :  
+   \`:::::   .'  
+     \`'::.-' 
+  `,
+  "Waning Crescent": `      _..._     
+    .::'   \`.    
+   :::       :    Waning Crescent
+   :::       :  
+   \`::.     .'  
+     \`':..-'    
+  
+  `,
+  "New Moon": `      _..._     
+    .'     \`.    
+   :         :    New Moon
+   :         :  
+   \`.       .'  
+     \`-...-'  
+  `,
+  "Waxing Crescent": `      _..._     
+    .'   \`::.    
+   :       :::    Waxing Crescent
+   :       :::  
+   \`.     .::'  
+     \`-..:'' 
+  `,
+  "First Quarter": `      _..._     
+    .'  ::::.    
+   :    ::::::    First Quarter
+   :    ::::::  
+   \`.   :::::'  
+     \`-.::''   
+  `,
+  "Waxing Gibbous": `     _..._     
+   .' .::::.    
+  :  ::::::::    Waxing Gibbous
+  :  ::::::::  
+  \`. '::::::'  
+    \`-.::'' 
+  `,
+};
---
+++ bin/index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+import { parse } from "node-html-parser";
+import chalk from "chalk";
+import { table } from "table";
+import { Command } from "commander";
+import { moonPhaseArt } from "./ascii.js";
+const program = new Command();
+
+program.name("moon-cli").description("Moon phase & status CLI application");
+program.option("-d, --date <date>");
+program.option("-f, --full");
+
+program.parse();
+
+console.log(program.opts());
+
+// const moonVisual = {
+//   "New Moon": `    _..._
+//   .:::::::.
+//  :::::::::::   NEW  MOON
+//  :::::::::::
+//  \`:::::::::'
+//    \`':::''
+// `,
+//   "Waxing Crescent": `    _..._
+//   .::::. \`.
+//  :::::::.  :    WAXING CRESCENT
+//  ::::::::  :
+//  \`::::::' .'
+//    \`'::'-'
+// `,
+//   "First Quarter": `    _..._
+//   .::::  \`.
+//  ::::::    :    FIRST QUARTER
+//  ::::::    :
+//  \`:::::   .'
+//    \`'::.-'
+// `,
+//   "Waxing Gibbous": `    _..._
+//   .::'   \`.
+//  :::       :    WAXING GIBBOUS
+//  :::       :
+//  \`::.     .'
+//    \`':..-'
+
+// `,
+//   "Full Moon": `    _..._
+//   .'     \`.
+//  :         :    FULL MOON
+//  :         :
+//  \`.       .'
+//    \`-...-'
+// `,
+//   "Waning Gibbous": `    _..._
+//   .'   \`::.
+//  :       :::    WANING GIBBOUS
+//  :       :::
+//  \`.     .::'
+//    \`-..:''
+// `,
+//   "Last Quarter": `    _..._
+//   .'  ::::.
+//  :    ::::::    LAST QUARTER
+//  :    ::::::
+//  \`.   :::::'
+//    \`-.::''
+// `,
+//   "Waning Crescent": `   _..._
+//  .' .::::.
+// :  ::::::::    WANING CRESCENT
+// :  ::::::::
+// \`. '::::::'
+//   \`-.::''
+// `,
+// };
+
+async function pullMoonData(type = "today") {
+  const res = await fetch(`https://www.moongiant.com/phase/${type}/`);
+  const html = await res.text();
+
+  const root = parse(html);
+  const summary = root.querySelector("#fullDateTitle > p").innerText.trim();
+  const moonDetails = root
+    .querySelector("#moonDetails")
+    .innerText.split("\n")
+    .map((detail) => detail.trim())
+    .filter((detail) => detail)
+    .map((detail) => detail.split(": "));
+
+  const date = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  console.log(chalk.white.bold(date.toLocaleDateString("en-US", options)));
+  console.log(moonPhaseArt[moonDetails[0][1]]);
+
+  console.log(chalk.blue.bold(`Details`));
+  console.log(table(moonDetails));
+  console.log(chalk.blue.bold(`\nSummary`));
+  const summaryTableConfig = {
+    columns: [{ width: 50, wrapWord: true }],
+  };
+
+  console.log(table([[summary]], summaryTableConfig));
+}
+
+// pullMoonData();
